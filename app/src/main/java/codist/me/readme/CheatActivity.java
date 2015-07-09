2e8e0e1aad83679e73a8611b93a18434fc7b52d1
yeeran <smartlyfly@gmail.com>
2015-07-09 08:15:55 +0800

    Chapter 5:Using Intent  pass extra param to another activity

---
+++ app/src/main/java/codist/me/readme/CheatActivity.java
@@ -0,0 +1,65 @@
+package codist.me.readme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CheatActivity extends Activity {
+
+    public static final String EXTRA__ANSWER_IS_TRUE = "codist.me.readme.geoquiz.anwser_is_true";
+
+    private boolean mAnswerIsTrue;
+
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA__ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+
+        mShowAnswer = (Button) findViewById(R.id.showAnswerButton);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_cheat, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/codist/me/readme/QuizActivity.java
+++ app/src/main/java/codist/me/readme/QuizActivity.java
@@ -1,6 +1,7 @@
 package codist.me.readme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -20,6 +21,7 @@ public class QuizActivity extends Activity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mCheatButton;
     private ImageButton mNextButton;
     private ImageButton mPrevButton;
     private TextView mQuestionTextView;
@@ -94,6 +96,17 @@ public class QuizActivity extends Activity {
                 }
             });
         }
+
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+                i.putExtra(CheatActivity.EXTRA__ANSWER_IS_TRUE, answerIsTrue);
+                startActivity(i);
+            }
+        });
     }
 
     private void updateQuestion() {
