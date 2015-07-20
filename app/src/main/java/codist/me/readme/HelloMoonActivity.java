f493b0df36a8054c750543d971fd1aca9330fcd5
yeeran <smartlyfly@gmail.com>
2015-07-21 00:02:32 +0800

    Chapter 13:  MediaPlayer doesn't work

--- app/src/main/java/codist/me/readme/HelloMoonActivity.java
+++ app/src/main/java/codist/me/readme/HelloMoonActivity.java
@@ -2,15 +2,16 @@ package codist.me.readme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class HelloMoonActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hello_moon);
     }
 
     @Override
